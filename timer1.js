0347d320b5007818c66a26fc22975b400fe58e26
huongw <whitneyhuong@gmail.com>
2021-08-24 19:34:01 +0000

    added a for loop to iterate through the args for setTimeout to run the beep, and implemented edge cases for NaN, negative integers, and empty args

---
+++ timer1.js
@@ -0,0 +1,16 @@
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('I am empty');
+} else {
+  for (let i = 0; i < args.length; i++) {
+    if (isNaN(args[i]) || args[i] < 0) {
+      continue;
+    }
+
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      console.log(`${args[i]} seconds`)
+    }, args[i] * 1000)
+  }  
+}
